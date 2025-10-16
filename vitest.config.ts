77a676a8809cc0abdd7be962a676dc0aaef24660
Iskhak Abdusalamov <iskhak_abdusalamov@internet.ru>
2025-10-16 07:48:49 +0500

    setup vitest

---
+++ vitest.config.ts
@@ -0,0 +1,25 @@
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      include: ['src/**/*.vi.test.ts'],
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      setupFiles: ['./vitest/setup.ts'],
+      coverage: {
+        // enabled: true,
+        provider: 'v8',
+        reportsDirectory: 'coverage',
+        reporter: ['text', 'html'],
+        include: ['src'],
+        exclude: ['src/**/*.stories.ts'],
+        all: true,
+      },
+    },
+  }),
+)
